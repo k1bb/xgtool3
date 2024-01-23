6f199c5fc9ac70c0bbf4fb97e52c8eef0d64ad15
Y.Takano <ytakano@weathermap.co.jp>
2024-01-23 10:25:55 +0900

    install_requiresを追加

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(
     author="Keiichi Hashimoto",
     author_email="k1bridgebook@g.ecc.u-tokyo.ac.jp",
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "xarray",
+        "pandas",
+        "dask",
+        "cftime",
+    ],
 )
